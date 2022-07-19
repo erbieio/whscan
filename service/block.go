09d9aaa83718d738a1ab371bfa716cb13e40e9e8
wen.lang <llfcjyy@outlook.com>
2022-07-19 18:13:18 +0800

    feat(*):Fixed bugs, Remove reward details at block list query, too slow.

--- service/block.go
+++ service/block.go
@@ -6,18 +6,12 @@ import (
 
 // BlocksRes block paging return parameters
 type BlocksRes struct {
-	Total  int64 `json:"total"` //The total number of blocks
-	Blocks []*struct {
-		model.Block
-		CoinCount uint64 `json:"coinCount"` //number of times to get coin rewards, 0.1ERB once
-		SNFTCount uint64 `json:"snftCount"` //number of times to get SNFT rewards
-	} `json:"blocks"` //block list
+	Total  int64         `json:"total"`  //The total number of blocks
+	Blocks []model.Block `json:"blocks"` //block list
 }
 
 func FetchBlocks(page, size int) (res BlocksRes, err error) {
-	err = DB.Model(&model.Block{}).Joins("RIGHT JOIN rewards ON block_number=number").
-		Group("number").Select("blocks.*, COUNT(amount) AS coin_count, COUNT(snft) AS snft_count").
-		Order("number DESC").Offset((page - 1) * size).Limit(size).Scan(&res.Blocks).Error
+	err = DB.Order("number DESC").Offset((page - 1) * size).Limit(size).Find(&res.Blocks).Error
 	// use cache to speed up queries
 	res.Total = int64(TotalBlock())
 	return
