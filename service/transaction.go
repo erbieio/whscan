4e7b809ffb7eb06986886cbc904a5f35521868fa
unknown <dev@erbie.io>
2024-10-31 14:55:18 +0800

    fix: modify the transaction details

--- service/transaction.go
+++ service/transaction.go
@@ -10,16 +10,16 @@ import (
 
 // TransactionDetail
 type TransactionsDetail struct {
-	tx          model.Transaction `json:"tx"`
-	TokenAmount types.BigInt      `json:"token_amount"`
+	model.Transaction
+	TokenAmount types.BigInt `json:"token_amount"`
 }
 
 func GetTransaction(hash string) (res TransactionsDetail, err error) {
-	err = DB.Model(&model.Transaction{}).Where("transactions.hash=?", hash).Take(&res.tx).Error
-	if err != nil {
+	err = DB.Model(&model.Transaction{}).Where("transactions.hash=?", hash).Scan(&res).Error
+	if err == nil {
 		var contractTx model.ContractTx
-		err2 := DB.Model(&model.ContractTx{}).Where("transactions.hash=?", hash).Take(&contractTx).Error
-		if err2 != nil {
+		err2 := DB.Model(&model.ContractTx{}).Where("hash=?", hash).Take(&contractTx).Error
+		if err2 == nil {
 			res.TokenAmount = contractTx.Value
 		}
 	}
