81afe5f2dd5da8a8a50c05f3cb7f9d8bceb5d034
unknown <dev@erbie.io>
2024-10-31 14:16:09 +0800

    perf: add token amount for transaction details

--- service/transaction.go
+++ service/transaction.go
@@ -8,8 +8,21 @@ import (
 	"time"
 )
 
-func GetTransaction(hash string) (res model.Transaction, err error) {
-	err = DB.Where("transactions.hash=?", hash).Take(&res).Error
+// TransactionDetail
+type TransactionsDetail struct {
+	model.Transaction
+	TokenAmount types.BigInt `json:"token_amount"`
+}
+
+func GetTransaction(hash string) (res TransactionsDetail, err error) {
+	err = DB.Model(&model.Transaction{}).Where("transactions.hash=?", hash).Take(&res).Error
+	if err != nil {
+		var contractTx model.ContractTx
+		err2 := DB.Model(&model.ContractTx{}).Where("transactions.hash=?", hash).Take(&contractTx).Error
+		if err2 != nil {
+			res.TokenAmount = contractTx.Value
+		}
+	}
 	return
 }
 
